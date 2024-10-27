17a120f551bb7a5bb5ccbcbdeacef5179b7112a9
Kuroneko <kannna04120405@yahoo.co.jp>
2024-10-27 13:04:28 +0900

    prefix changed2

--- discord_template.py
+++ discord_template.py
@@ -2,10 +2,10 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.all()
-discordpypy = commands.Bot(command_prefix="!", intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 @discordpypy.event
 async def on_ready():
     print("ボットが死んでしまった！")
 
-discordpypy.run("TOKEN") ##他人にTOKENを共有すると、荒らしに使われる可能性があります
+client.run("TOKEN") ##他人にTOKENを共有すると、荒らしに使われる可能性があります
